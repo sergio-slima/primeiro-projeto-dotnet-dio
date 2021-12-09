183c6d327c7bde473c2021e78db0c7d6136c52dd
sergio-slima <sergio_silvalima@hotmail.com>
2021-12-09 13:20:50 -0300

    primeiro projeto

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PrimeiroProjeto
+{
+    class Program
+    {
+      static void Main(string[] args)
+      {
+          int numero = 10;
+        for (int i=0; i < numero; i++) {
+          Console.WriteLine("Hello, World " + i);
+        }
+      }
+    }
+}
+
+
+
+
